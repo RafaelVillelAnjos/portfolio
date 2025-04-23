c11e0edefd0f2bcbaddcff634bea5c0282cbe5fb
RafaelVillelAnjos <161859121+RafaelVillelAnjos@users.noreply.github.com>
2025-04-22 22:29:48 -0300

    Add files via upload

---
+++ Ensino-Medio/EM.js
@@ -0,0 +1,22 @@
+// Botões do cabeçalho
+function IrParaIni(){
+    window.location.href = "../index/index.html"
+}
+
+// Botões das matérias
+function IrParaMtm(){
+    window.location.href = "Matematica/Mtm.html"
+}
+
+function IrParaHumanas(){
+    window.location.href =  "Humanas/HM.html"
+}
+
+function IrParaLinguagens(){
+    window.location.href =  "Linguagens/Lin.html"
+}
+
+function IrParaNatureza(){
+    window.location.href =  "Natureza/Nat.html"
+}
+
---
+++ PPE/PPE.js
@@ -0,0 +1,3 @@
+function Voltar(){
+    window.location.href = "../index/index.html"
+}
